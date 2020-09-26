806b94139feedd4dfbc8150255f6bd994256d39a
Zing <yuenzhiying@gmail.com>
2020-09-26 14:44:52 +0800

    Create element using web api

---
+++ src/webApi.js
@@ -0,0 +1,9 @@
+function addElement() {
+    const h1 = document.createElement("h1");
+    const title = document.createTextNode("Learn React");
+
+    h1.appendChild(title);
+
+    const currentDiv = document.getElementById("app");
+    document.body.insertBefore(h1, currentDiv)
+}

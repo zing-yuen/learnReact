32e5ab0947c6e021b4692cab05d4e08de6044cd3
Zing <yuenzhiying@gmail.com>
2020-09-26 15:26:46 +0800

    Append children

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const title = React.createElement('h1', {}, 'Learn React');
 
+const paragraph = React.createElement('p', {}, 'Hands-on practice with Zing')
+
+const container = React.createElement('div', {}, [paragraph, title]);
+
 ReactDOM.render(
-    title,
+    container,
     document.getElementById('app')
 );

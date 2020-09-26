73fbbb7c6ece1b8ef813750c693a0d796ab4dc6d
Zing <yuenzhiying@gmail.com>
2020-09-26 17:00:59 +0800

    Data flow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const ce = React.createElement;
 
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return `${this.props.name} liked this.`;
+        }
+
+        return ce(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -10,9 +29,11 @@ class App extends React.Component {
         const title = ce('h1', {}, 'Learn React');
         const paragraph = ce('p', {}, 'Hands-on practice with Zing')
 
+        const likeButton = ce(LikeButton, {name: this.state.name}, null)
+
         return ce(
             'div', {},
-            [title, paragraph]
+            [title, paragraph, likeButton]
         )
     }
 }
